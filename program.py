105d2645fbd60bfd513f35c21956899dfc25a36b
Patrick Hess <patrickhess@patrickhess-macbookpro.roam.corp.google.com>
2016-03-20 22:58:27 -0700

    Add logic for tf-idf from descriptions

--- program.py
+++ program.py
@@ -36,27 +36,26 @@ stemmer = SnowballStemmer('english')
 
 word_set = set([stemmer.stem(word) for word in words.words()])
 ids_have_word = {}
+documents_its_in = {}
+
+num_documents = 0
 
 def words_in_common(words1, words2):
     sum = 0
     dct = set()
     for word1 in words1:
-        if word1 == 'in.':
-            word1 = 'inch'
         for word2 in words2:
-            if word1 == 'in.':
-                word2 = 'inch'
             if word1 not in dct and word2 not in dct and word2.find(word1) >= 0:
                 dct.add(word1)
                 dct.add(word2)
-                sum += 1
+                sum += math.log(num_documents / documents_its_in[word1])
     return sum
 
 def edit_dist_less_two(words1, words2):
     sum = 0
     for word1 in words1:
         for word2 in words2:
-            if len(word1) > 5 and len(word2) > 5 and editdistance.eval(word1, word2) <= 2:
+            if len(word1) > 5 and len(word2) > 5 and editdistance.eval(word1, word2) <= 2 and word1[0] == word2[0]:
                 sum += 1
                 break
     return sum
@@ -84,15 +83,18 @@ def generate_features(raw_train):
         data_point.append(len(row['search_term']))
         data_point.append(len(row['product_title']))
         search_stemmed = [''.join(e for e in stemmer.stem(word) if e.isalnum()) for word in row['search_term'].split() if stemmer.stem(word)]
-        search_stemmed = ['in.' if w == 'inch' else w for w in search_stemmed]
-        search_stemmed = ['ft.' if w == 'feet' else w for w in search_stemmed]
+        search_stemmed = ['inch' if w == 'in.' else w for w in search_stemmed]
+        search_stemmed = ['ft' if (w == 'feet' or w == 'foot') else w for w in search_stemmed]
+        search_stemmed = ['lb' if (w == 'pound' or w == 'pounds') else w for w in search_stemmed]
         title_stemmed = [''.join(e for e in stemmer.stem(word) if e.isalnum()) for word in row['product_title'].split() if stemmer.stem(word)]
+        title_stemmed = ['inch' if w == 'in.' else w for w in title_stemmed]
+        title_stemmed = ['ft' if (w == 'feet' or w == 'foot') else w for w in title_stemmed]
+        title_stemmed = ['lb' if (w == 'pound' or w == 'pounds') else w for w in title_stemmed]
         data_point.append(words_in_common(search_stemmed, title_stemmed))
         data_point.append(words_in_common(search_stemmed, descr_dict[pd_id]))
         data_point.append(edit_dist_less_two(search_stemmed, title_stemmed))
         data_point.append(edit_dist_less_two(search_stemmed, descr_dict[pd_id]))
         data_point.append(1 if search_stemmed[-1] in title_stemmed else 0)
-        data_point.append(weighted_count(search_stemmed, descr_dict[pd_id]))
         search_joined = "".join([word for word in search_stemmed])
         title_joined = "".join([word for word in title_stemmed])
         data_point.append(len(search_stemmed) if (title_joined.find(search_joined) >= 0 and len(search_stemmed) > 1) else 0)
@@ -124,7 +126,18 @@ descr_dict = {}
 descr_len = {}
 # Stem descriptions
 for it, row in descriptions_raw.iterrows():
-    descr_dict[row['product_uid']] = [''.join(e for e in stemmer.stem(word) if e.isalnum()) for word in row['product_description'].split()]
+    words = [''.join(e for e in stemmer.stem(word) if e.isalnum()) for word in row['product_description'].split()]
+    descr_dict[row['product_uid']] = words
+    x = set()
+    for w in words:
+        if w not in x:
+            if w in documents_its_in:
+                documents_its_in[w] += 1
+            else:
+                documents_its_in[w] = 1
+        x.add(w)
+
+    num_documents += 1
 
 X = generate_features(raw_train)
 Xtest = generate_features(raw_test)
@@ -150,7 +163,7 @@ def build_model(input_dim, hn=64, dp=0.5, layers=1):
     return model
 
 
-model = build_model(len(X[0]), 48, 0.35, 1)
+model = build_model(len(X[0]), 64, 0.35, 1)
 model.fit(X, Y, nb_epoch=32, batch_size=84, verbose=2)
 #rfc = RandomForestRegressor(n_estimators=100, min_samples_split=10)
 #rfc.fit(X, Y)
@@ -159,27 +172,29 @@ model.fit(X, Y, nb_epoch=32, batch_size=84, verbose=2)
 # Classifier
 model2 = xgb.XGBRegressor()
 params = {
-        'n_estimators': [100, 150, 250],
-        'learning_rate': [0.05, 0.1, 0.3],
-        'max_depth': [3, 6, 9, 12],
-        'subsample': [0.25, 0.35, 0.5, 0.7],
-        'colsample_bytree': [0.3, 0.5, 0.75, 0.9]}
+        'n_estimators': [133, 134, 135, 136],
+        'learning_rate': [0.04],
+        'max_depth': [8],
+        'subsample': [0.88],
+        'colsample_bylevel': [0.9, 0.92, 0.95],
+        'colsample_bytree': [0.78],
+         'min_child_weight': [3]}
 
-clf1 = GridSearchCV(model2, params, verbose=1)
+clf1 = GridSearchCV(model2, params, verbose=1, n_jobs=8, cv=7)
 clf1.fit(X, Y)
 print(clf1.best_score_)
 print(clf1.best_params_)
 
-num_boost_round = 112
+best_params = {'subsample': 0.7, 'learning_rate': 0.05, 'colsample_bytree': 0.75, 'max_depth': 9}
+
+num_boost_round = 120
 dtrain = xgb.DMatrix(X, Y)
-#clf1 = xgb.cv(params=params, dtrain=dtrain, num_boost_round=num_boost_round, early_stopping_rounds=10, nfold=5)
-clf1 = xgb.train(params=params, dtrain=dtrain, num_boost_round=num_boost_round)
+#clf1 = xgb.cv(params=best_params, dtrain=dtrain, num_boost_round=num_boost_round, early_stopping_rounds=10, nfold=5)
+clf2 = xgb.train(params=params, dtrain=dtrain, num_boost_round=num_boost_round)
 #clf1.predict(xgb.DMatrix(X))
 math.sqrt(mean_squared_error(Y[60000:], clf1.predict(xgb.DMatrix(X[60000:]))))
 
-Ypred = [max(1, min(3, y)) for y in clf1.predict(xgb.DMatrix(Xtest))]
-Ypred = numpy.add(Ypred, [y[0] for y in model.predict(Xtest)])
-Ypred /= 2
+Ypred = [0.7 * max(1, min(3, y)) for y in clf1.predict(Xtest)]
+Ypred = numpy.add(Ypred, [y[0] * 0.3 for y in model.predict(Xtest)])
 id_test = raw_test['id']
 pandas.DataFrame({"id": id_test, "relevance": Ypred}).to_csv('submission.csv',index=False)
-
