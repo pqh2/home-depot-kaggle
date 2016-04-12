5c61f6e4903c8e32d08c67fbe85b9df4705c9877
Patrick Hess <patrickhess@patrickhess-macbookpro.roam.corp.google.com>
2016-04-12 14:17:26 -0700

    Add difference (other than just ratio) between matches and search length

--- program.py
+++ program.py
@@ -3,7 +3,7 @@ import gzip
 ###
 import numpy as np
 ###
-
+from sklearn.linear_model import LinearRegression
 from sklearn.grid_search import GridSearchCV
 from sklearn.ensemble import BaggingRegressor
 from keras.layers.advanced_activations import PReLU, LeakyReLU
@@ -65,6 +65,8 @@ def word_to_vec_score(words1, words2):
             if word2.find(word1) == 0 and mod1 in wordToVec.vocab and mod2 in wordToVec.vocab and wordToVec.similarity(mod1, mod2) > 0:
                 if word1 in documents_its_in:
                     sum += wordToVec.similarity(mod1, mod2)**2 * (math.log(num_documents / documents_its_in[word1]))
+                else:
+                    sum += wordToVec.similarity(mod1, mod2)**2
     return sum
 
 def edit_dist_less_two(words1, words2):
@@ -106,8 +108,16 @@ def generate_features(raw_train):
         title_stemmed = [''.join(e for e in stemmer.stem(word) if e.isalnum()) for word in row['product_title'].split() if stemmer.stem(word)]
         title_stemmed = ['in.' if w == 'inch' else w for w in title_stemmed]
         title_stemmed = ['ft.' if w == 'feet' else w for w in title_stemmed]
+        title_before_with = []
+        for w in title_stemmed:
+            title_before_with.append(w)
+            if w == 'with':
+                break
         #title_stemmed = ['lb.' if w == 'pound' else w for w in title_stemmed]
         data_point.append(word_to_vec_score(search_stemmed, title_stemmed) / len(search_stemmed))
+        data_point.append(words_in_common(search_stemmed, title_before_with) - len(search_stemmed))
+        data_point.append(words_in_common(search_stemmed, descr_dict[pd_id]) - len(search_stemmed))
+        #data_point.append(words_in_common(search_stemmed, title_before_with) / len(search_stemmed))
         data_point.append(words_in_common(search_stemmed, title_stemmed) / len(search_stemmed))
         data_point.append(words_in_common(search_stemmed, descr_dict[pd_id]) / len(search_stemmed))
         data_point.append(edit_dist_less_two(search_stemmed, title_stemmed))
@@ -176,15 +186,15 @@ model1.fit(X, Y, nb_epoch=32, batch_size=84, verbose=2)
 
 model2 = build_model(len(X[0]), 64, 0.35, 2, 'glorot_normal')
 model2.fit(X, Y, nb_epoch=32, batch_size=84, verbose=2)
-rfc = RandomForestRegressor(n_estimators=1000, min_samples_split=20)
-rfc.fit(X, Y)
-
+rfc1 = RandomForestRegressor(n_estimators=1200, min_samples_split=20, max_depth=50)
+rfc1.fit(X, Y)
 param_grid = {
-    'n_estimators': [1000],
+    'n_estimators': [1200],
     "min_samples_split": [20],
+    'max_depth': [50]
 }
 
-CV_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv= 5, n_jobs=4)
+CV_rfc = GridSearchCV(estimator=rfc1, param_grid=param_grid, cv= 5, n_jobs=4)
 
 CV_rfc.fit(X, Y)
 
@@ -194,19 +204,36 @@ CV_rfc.fit(X, Y)
 # Classifier
 model2 = xgb.XGBRegressor()
 params = {
-        'n_estimators': [645, 650, 655],
-        'learning_rate': [0.04],
-        'max_depth': [10],
-         'subsample': [0.9, 0.92, 0.93],
-        'colsample_bylevel': [0.75],
-        'colsample_bytree': [0.7, 0.72]
+        'n_estimators': [250, 500, 750],
+        'learning_rate': [0.03, 0.04, 0.05],
+        'max_depth': [8, 10, 11, 12],
+         'subsample': [0.8, 0.93],
+        'colsample_bylevel': [0.65, 0.75, 0.85],
+        'colsample_bytree': [0.6, 0.7, 0.8]
 }
+
+#best_params = {'colsample_bytree': 0.7, 'colsample_bylevel': 0.75, 'learning_rate': 0.04, 'n_estimators': 500, 'subsample': 0.93, 'max_depth': 10}
 clf1 = GridSearchCV(model2, params, verbose=1, n_jobs = 4, cv=7)
 clf1.fit(X, Y)
 print(clf1.best_score_)
 print(clf1.best_params_)
 
 
+model2 = xgb.XGBRegressor()
+params = {
+        'n_estimators': [700, 800, 900],
+        'learning_rate': [0.04],
+        'max_depth': [9],
+         'subsample': [0.9],
+        'colsample_bylevel': [0.75],
+        'colsample_bytree': [0.7]
+}
+clf2 = GridSearchCV(model2, params, verbose=1, n_jobs = 4, cv=7)
+clf2.fit(X, Y)
+print(clf2.best_score_)
+print(clf2.best_params_)
+
+"""
 # Classifier
 model2 = xgb.XGBRegressor()
 params = {
@@ -238,29 +265,34 @@ clf3 = GridSearchCV(model2, params, verbose=1, n_jobs=4, cv=7)
 clf3.fit(X, Y)
 print(clf3.best_score_)
 print(clf3.best_params_)
+"""
 
+Xblended = np.column_stack((clf1.predict(X), clf2.predict(X), rfc1.predict(X)))
 
-Xoutput = np.column_stack((model1.predict(X), clf1.predict(X), clf2.predict(X)))
-
-model2 = build_model(len(Xoutput[0]), 16, 0.85, 1, 'glorot_normal')
-model2.fit(Xoutput, Y, nb_epoch=32, batch_size=32, verbose=2)
-
+rfc2 = RandomForestRegressor(n_estimators=100)
+rfc2.fit(Xblended, Y)
+linModel = LinearRegression()
+linModel.fit(Xblended, Y)
 num_boost_round = 400
 dtrain = xgb.DMatrix(X, Y)
 best_params = {'colsample_bytree': 0.78, 'colsample_bylevel': 0.9, 'learning_rate': 0.05, 'min_child_weight': 3, 'n_estimators': 136, 'subsample': 0.88, 'max_depth': 9}
 #clf1 = xgb.cv(params=params, dtrain=dtrain, num_boost_round=num_boost_round, early_stopping_rounds=10, nfold=5)
 clf1 = xgb.train(params=best_params, dtrain=dtrain, num_boost_round=num_boost_round)
 #clf1.predict(xgb.DMatrix(X))
-math.sqrt(mean_squared_error(Y[60000:], clf1.predict(X[60000:])))
+math.sqrt(mean_squared_error(Y[60000:], rfc1.predict(X[60000:])))
 
 
+Xblended = np.column_stack((clf1.predict(Xtest), clf2.predict(Xtest), rfc1.predict(Xtest)))
+Ypred = linModel.predict(Xblended)
+Ypred2 = rfc2.predict(Xblended)
 #Xoutput2 = np.column_stack((model1.predict(Xtest), clf1.predict(Xtest), clf2.predict(Xtest)))
 #Ypred = model2.predict(Xoutput2)
-Ypred = [0.5 * max(1, min(3, y)) for y in clf1.predict(Xtest)]
-#Ypred = numpy.add(Ypred, [0.4 * max(1, min(3, y)) for y in clf2.predict(Xtest)])
+Ypred = [0.3 * max(1, min(3, y)) for y in clf1.predict(Xtest)]
+Ypred = numpy.add(Ypred, [0.1 * max(1, min(3, y)) for y in clf2.predict(Xtest)])
 #Ypred = numpy.add(Ypred, [0.2 * y for y in clf3.predict(Xtest)])
-Ypred = numpy.add(Ypred, [0.35 * y for y in rfc.predict(Xtest)])
-Ypred = numpy.add(Ypred, [0.15 * y[0] for y in model1.predict(Xtest)])
+Ypred = numpy.add(Ypred, [0.4 * y for y in rfc1.predict(Xtest)])
+Ypred = numpy.add(Ypred, [0.1 * y[0] for y in model1.predict(Xtest)])
+Ypred = numpy.add(Ypred, [0.1 * y[0] for y in model2.predict(Xtest)])
 #Ypred = numpy.add(Ypred, [0.1 * y[0] for y in model2.predict(Xtest)])
 id_test = raw_test['id']
 pandas.DataFrame({"id": id_test, "relevance": Ypred}).to_csv('submission.csv',index=False)
